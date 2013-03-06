d9a67f00db36d928d86988d580bc93107527b864
RyanDurling <ryan.p.durling@gmail.com>
2013-03-06 13:07:25 -0800

    add wall service

---
+++ Wall/wall/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wall::Application.config.secret_token = '1d9e3cd9ede4a1631e68a03145bb0c1922426cc13be90fe41fa0de34879ff348cc932e6e8080369a1520969434684c0d45f913dfb91ad48b39d372d881922e56'
