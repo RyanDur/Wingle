6c67410914232708b4e34823d41dac4563956ac8
Sudhindra Rao [betarelease] <sudhindra.r.rao@gmail.com>
2013-04-04 12:18:41 -0700

    my changes

--- ng_wingle/app/scripts/controllers/Wall.js
+++ ng_wingle/app/scripts/controllers/Wall.js
@@ -5,3 +5,36 @@ angular.module('ngWingleApp')
 	$scope.cards = Cards.getCards();
 	$scope.cardsCtrl = Cards;
     });
+
+
+    angular.module('plunker', ['ui.bootstrap']);
+
+    function AlertDemoCtrl($scope) {
+
+      $scope.alerts = [
+
+        { type: 'error', msg: 'Oh snap! Change a few things up and try submitting again.' }, 
+
+        { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+
+      ];
+
+
+
+      $scope.addAlert = function() {
+
+        $scope.alerts.push({msg: "Another alert!"});
+
+      };
+
+
+
+      $scope.closeAlert = function(index) {
+
+        $scope.alerts.splice(index, 1);
+
+      };
+
+
+
+    }
