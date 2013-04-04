0fe8abed2e347573fa06fda0fbaf29963fb8b837
Sudhindra Rao [betarelease] <sudhindra.r.rao@gmail.com>
2013-04-03 19:42:11 -0700

    angular ui and bootstrap are now working - not throwing weird errors anymore

--- ng_wingle/app/scripts/services/Cards.js
+++ ng_wingle/app/scripts/services/Cards.js
@@ -5,25 +5,20 @@ function puts(){
 }
 angular.module('ngWingleApp')
     .factory('Cards', function ($resource) {
-	var Card_resource = $resource("http://localhost\\:4567/card");
-        Card_resource.get();
-
-puts(Card_resource);
-//   var i = 1;
-//	var cards = Card_resource.query();
+      var Card_resource = $resource("http://localhost\\:4567/card");
+      var card = Card_resource.get();
 
         // Public API here
         return {
             getCards: function() {
-		return Card_resource.get();
-               //return cards;
+               return card;
             },
             addCard: function (card) {
-/*                card = Card_resource.save(card, function(){}, function error(response){
+                card = Card_resource.save(card, function(){}, function error(response){
                     return errors = response.data.errors;
                 });
                 cards.push(card);
-                return card = {};*/
+                return card = {};
             }
         };
     });
