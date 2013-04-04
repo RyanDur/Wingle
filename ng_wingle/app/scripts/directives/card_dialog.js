660687c90fbe9a35b632e0a08a16cf5a17e6af12
RyanDurling <ryan.p.durling@gmail.com>
2013-04-04 08:53:30 -0700

    directive dialog work

--- ng_wingle/app/scripts/directives/card_dialog.js
+++ ng_wingle/app/scripts/directives/card_dialog.js
@@ -1,28 +1,20 @@
 'use strict';
 
 angular.module('ngWingleApp')
-  .directive('cardDialog', function () {
-    return {
-/*            transclude: true,
-            scope: {
+    .directive('cardDialog', function () {
+        return {
+	    restrict: 'E',
+         //   transclude: true,
+         /*   scope: {
                 title: '@',             // the title uses the data-binding from the parent scope
                 onOk: '&',              // create a delegate onOk function
                 onCancel: '&',          // create a delegate onCancel function
                 visible: '='            // set up visible to accept data-binding
-            },
-            template: '<div ng-show="visible">'+
-                        '<h3>{{title}}</h3>'+
-                        '<div class="body" ng-transclude></div>'+
-                        '<div class="footer">'+
-                          '<button ng-click="onOk()">Save changes</button>'+
-                          '<button ng-click="onCancel()">Close</button>'+
-                        '</div>'+
-                      '</div>',
-            restrict: 'E',
-            replace: true,
-            link: function postLink(scope, element, attrs) {
+            },*/
+            templateUrl: 'views/partials/dialog.html'
+//            replace: true,
+  /*          link: function postLink(scope, element, attrs) {
                 element.text('this is the dialog directive');
-            }
-        };*/
-    };
-  });
+            }*/
+        };
+    });
