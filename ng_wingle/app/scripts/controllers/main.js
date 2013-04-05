dfa3fbe101c97515d08371aa0227c04216099db9
RyanDurling <ryan.p.durling@gmail.com>
2013-04-05 16:27:38 -0700

    add drop down for research in nav bar

--- ng_wingle/app/scripts/controllers/main.js
+++ ng_wingle/app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('ngWingleApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+        $scope.dropdown = [
+            {text: 'Egghead', href: 'http://egghead.io/'},
+            {text: 'BootstrapCDN', href: 'http://www.bootstrapcdn.com/'}
+        ];
+
+    });
